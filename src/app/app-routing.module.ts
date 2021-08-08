0a28a195424fd3bc5bda3a2b7e9b81e23261a0b3
Rikesh <recasechhunju@gmail.com>
2021-08-08 21:14:24 +0545

    update app route and module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule) }, { path: 'live', loadChildren: () => import('./live-stock/live-stock.module').then(m => m.LiveStockModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
