c63d6431accbef865113fb28df8f9c892b130c92
Rikesh <recasechhunju@gmail.com>
2021-08-12 21:41:13 +0545

    Update stock module

--- src/app/live-stock/live-stock-routing.module.ts
+++ src/app/live-stock/live-stock-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LiveComponent } from './components/live/live.component';
 import { LiveStockComponent } from './live-stock.component';
 
-const routes: Routes = [{ path: '', component: LiveStockComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LiveStockComponent,
+    children: [{ path: '', component: LiveComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class LiveStockRoutingModule { }
+export class LiveStockRoutingModule {}
--- src/app/live-stock/live-stock.module.ts
+++ src/app/live-stock/live-stock.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LiveStockRoutingModule } from './live-stock-routing.module';
 import { LiveStockComponent } from './live-stock.component';
 import { LiveComponent } from './components/live/live.component';
-
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    LiveStockComponent,
-    LiveComponent
-  ],
-  imports: [
-    CommonModule,
-    LiveStockRoutingModule
-  ]
+  declarations: [LiveStockComponent, LiveComponent],
+  imports: [CommonModule, LiveStockRoutingModule, MatTableModule],
 })
-export class LiveStockModule { }
+export class LiveStockModule {}
