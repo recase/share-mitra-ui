3c59ab8975dfe4e7f9a4c1638a2fd119726d5f56
Rikesh <recasechhunju@gmail.com>
2021-08-12 21:41:59 +0545

    Add design for live data table

--- src/app/live-stock/components/live/live.component.ts
+++ src/app/live-stock/components/live/live.component.ts
@@ -3,13 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-live',
   templateUrl: './live.component.html',
-  styleUrls: ['./live.component.scss']
+  styleUrls: ['./live.component.scss'],
 })
 export class LiveComponent implements OnInit {
+  public liveData = [
+    {
+      symbol: 'nambi',
+      lastTradedPrice: 23,
+      change: 0.3,
+      previousClose: 23,
+      openPrice: 23,
+      highPrice: 32,
+      lowPrice: 21,
+      lastTradedVolume: 32,
+      lastTradedTime: new Date().toLocaleString(),
+      totalVolume: 300,
+    },
+    {
+      symbol: 'nambi',
+      lastTradedPrice: 23,
+      change: -0.3,
+      previousClose: 23,
+      openPrice: 23,
+      highPrice: 32,
+      lowPrice: 21,
+      lastTradedVolume: 32,
+      lastTradedTime: new Date().toLocaleString(),
+      totalVolume: 300,
+    },
+    {
+      symbol: 'nambi',
+      lastTradedPrice: 23,
+      change: 0.0,
+      previousClose: 23,
+      openPrice: 23,
+      highPrice: 32,
+      lowPrice: 21,
+      lastTradedVolume: 32,
+      lastTradedTime: new Date().toLocaleString(),
+      totalVolume: 300,
+    },
+  ];
+  public displayedColumns = [
+    'symbol',
+    'ltp',
+    'change',
+    'pclose',
+    'open',
+    'high',
+    'low',
+    'ltv',
+    'ltt',
+    'tv',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
