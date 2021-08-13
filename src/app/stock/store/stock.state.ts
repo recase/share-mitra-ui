45df97f7e82cfa76e937ed7c19338a68d5259daf
Rikesh <recasechhunju@gmail.com>
2021-08-13 17:11:11 +0545

    Add stock state

---
+++ src/app/stock/store/stock.state.ts
@@ -0,0 +1,10 @@
+import { StockState } from 'src/app/interface';
+
+export const stockState: StockState = {
+  companies: [],
+  sectors: [],
+  stockPriceList: undefined,
+  StockPriceDateLoading: false,
+  compantDataLoading: false,
+  sectorDataLoading: false,
+};
