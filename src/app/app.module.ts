feb37463d49e26c666abb650fef2efc6ebde645d
Rikesh <recasechhunju@gmail.com>
2021-08-13 17:10:46 +0545

    Add ngrx store module for state management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/component/nav-bar/nav-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { KeyFormattingInterceptor } from './interceptors/key-formatting.interceptor';
+import { StoreModule } from '@ngrx/store';
+
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { appReducer } from './store/app-reducer';
 
 @NgModule({
   declarations: [AppComponent, NavBarComponent],
@@ -15,6 +20,12 @@ import { KeyFormattingInterceptor } from './interceptors/key-formatting.intercep
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    StoreModule.forRoot(appReducer),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [
     {
