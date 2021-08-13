9a6f17c6c338a31b31ad9f2c3949eddf2fbf89cf
Rikesh <recasechhunju@gmail.com>
2021-08-13 17:31:46 +0545

    Add stock selectors

---
+++ src/app/stock/store/stock.selectors.ts
@@ -0,0 +1,27 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { StockState } from 'src/app/interface';
+export const STOCK_STATE = 'stock';
+
+const getStockSelector = createFeatureSelector<StockState>(STOCK_STATE);
+
+export const companiesSelector = createSelector(getStockSelector, (state) => {
+  return state.companies;
+});
+
+export const sectorSelector = createSelector(getStockSelector, (state) => {
+  return state.sectors;
+});
+
+export const stockPriceSelector = createSelector(getStockSelector, (state) => {
+  return state.stockPriceList;
+});
+
+export const companyLoading = createSelector(getStockSelector, (state) => {
+  return state.compantDataLoading;
+});
+export const sectorLoading = createSelector(getStockSelector, (state) => {
+  return state.sectorDataLoading;
+});
+export const stockPriceLoading = createSelector(getStockSelector, (state) => {
+  return state.StockPriceDateLoading;
+});
