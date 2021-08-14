149862e2ccdf02d7ceff9e089d88eb286920dcbc
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:21:10 +0545

    update module to have effect module

--- src/app/live-stock/live-stock.module.ts
+++ src/app/live-stock/live-stock.module.ts
@@ -5,9 +5,16 @@ import { LiveStockRoutingModule } from './live-stock-routing.module';
 import { LiveStockComponent } from './live-stock.component';
 import { LiveComponent } from './components/live/live.component';
 import { MatTableModule } from '@angular/material/table';
+import { EffectsModule } from '@ngrx/effects';
+import { LivePriceEffect } from './state/live.effects';
 
 @NgModule({
   declarations: [LiveStockComponent, LiveComponent],
-  imports: [CommonModule, LiveStockRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    LiveStockRoutingModule,
+    MatTableModule,
+    EffectsModule.forFeature([LivePriceEffect]),
+  ],
 })
 export class LiveStockModule {}
