b2d0f6251fd61611fb2024c5c528a554378b974c
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:08:13 +0545

    Update app reducer with live data reduces

--- src/app/store/app-reducer.ts
+++ src/app/store/app-reducer.ts
@@ -1,6 +1,9 @@
+import { liveReducer } from '../live-stock/state/live.reducers';
+import { LIVE_PRICE_STATE } from '../live-stock/state/live.selectors';
 import { stockReducer } from '../stock/store/stock.reducers';
 import { STOCK_STATE } from '../stock/store/stock.selectors';
 
 export const appReducer = {
   [STOCK_STATE]: stockReducer,
+  [LIVE_PRICE_STATE]: liveReducer,
 };
