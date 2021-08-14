6543b232df953896e52252b912424a5d658f3ce8
Rikesh <recasechhunju@gmail.com>
2021-08-14 22:23:59 +0545

    fix formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'stock', loadChildren: () => import('./stock/stock.module').then(m => m.StockModule) }, { path: 'live', loadChildren: () => import('./live-stock/live-stock.module').then(m => m.LiveStockModule) }];
+const routes: Routes = [
+  {
+    path: 'stock',
+    loadChildren: () =>
+      import('./stock/stock.module').then((m) => m.StockModule),
+  },
+  {
+    path: 'live',
+    loadChildren: () =>
+      import('./live-stock/live-stock.module').then((m) => m.LiveStockModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
