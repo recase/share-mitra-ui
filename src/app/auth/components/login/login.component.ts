eb9d78c42e1b712f34d4228e3d840eea558fc86e
Rikesh <recasechhunju@gmail.com>
2021-08-17 14:15:16 +0545

    Update methods to call auth actions to make login api call

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,24 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormBuilder,
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { AuthState } from 'src/app/interface';
+import { loginAction } from '../../state/auth.actions';
+import { loginErrorMsgSelector } from '../../state/auth.selectors';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   public loginFormGroup!: FormGroup;
   public hasFormError = false;
+  public responseError = false;
+  public apiErrorSubscription!: Subscription;
+  public errors!: string | null;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AuthState>,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.initializeLoginForm();
+    this.apiErrorSubscription = this.store
+      .select(loginErrorMsgSelector)
+      .subscribe((error) => {
+        this.errors = error;
+      });
   }
 
   private initializeLoginForm(): void {
@@ -33,8 +51,13 @@ export class LoginComponent implements OnInit {
       this.hasFormError = true;
       return;
     }
-
-    console.log(this.loginFormGroup);
+    this.responseError = true;
+    this.store.dispatch(
+      loginAction({
+        email: this.loginFormGroup.value.email,
+        password: this.loginFormGroup.value.password,
+      })
+    );
   }
 
   get loginFrom(): { [key: string]: AbstractControl } {
@@ -43,5 +66,15 @@ export class LoginComponent implements OnInit {
 
   public resetError(): void {
     this.hasFormError = false;
+  }
+
+  public navigateToSignup(): void {
+    this.router.navigate(['auth', 'signup']);
+  }
+
+  ngOnDestroy(): void {
+    if (this.apiErrorSubscription) {
+      this.apiErrorSubscription.unsubscribe();
+    }
   }
 }
