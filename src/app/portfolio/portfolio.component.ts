f8d78460f9063b4baab3e3a1bcf643e21955b6fd
Rikesh <recasechhunju@gmail.com>
2021-08-19 21:42:10 +0545

    Make actions call to initiate API call

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { PortfolioState } from '../interface';
+import { retrievePortfolioData } from './store/portfolio.actions';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
-
-  constructor() { }
+  constructor(private store: Store<PortfolioState>) {}
 
   ngOnInit(): void {
+    this.store.dispatch(retrievePortfolioData());
   }
-
 }
