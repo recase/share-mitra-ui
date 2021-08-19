885b756086d911253e3202c626ecd04212d81438
Rikesh <recasechhunju@gmail.com>
2021-08-19 17:38:24 +0545

    Add transaction type enum

---
+++ src/app/enums.ts
@@ -0,0 +1,10 @@
+export enum TransactionType {
+  IPO = 'IPO',
+  FPO = 'FPO',
+  Right = 'right',
+  Auction = 'auction',
+  Bonus = 'bonus',
+  Dividend = 'dividend',
+  BUY = 'secondary buy',
+  SELL = 'sell',
+}
