4218bba047863e1b9e84247ba71b92c84fcb0803
Rikesh <recasechhunju@gmail.com>
2021-08-19 17:45:40 +0545

    Udpate portfolio card with updated interface data

--- src/app/portfolio/shared/component/card/card.component.ts
+++ src/app/portfolio/shared/component/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PortfolioDetail } from 'src/app/interface';
+import { Portfolio } from 'src/app/interface';
 
 @Component({
   selector: 'app-card',
@@ -7,20 +7,7 @@ import { PortfolioDetail } from 'src/app/interface';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-  @Input() public portfolio: PortfolioDetail = {
-    symbol: '',
-    name: '',
-    totalStock: 0,
-    lastTradedPrice: 0,
-    totalStockPrice: 0,
-    stockType: '',
-    totalInvestment: 0,
-    totalStockInvestment: 0,
-    profitPercentage: 0,
-    overallProfit: 0,
-    change: 0,
-    todaysProfit: 0,
-  };
+  @Input() public portfolio!: Portfolio;
 
   constructor() {}
 
