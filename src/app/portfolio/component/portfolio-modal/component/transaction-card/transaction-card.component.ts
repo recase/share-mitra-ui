e84d26ceb8eb3b544a5b4f8aa7414c4470674741
Rikesh <recasechhunju@gmail.com>
2021-08-21 23:37:29 +0545

    Update methods to open modal to edit and delete the transactions

--- src/app/portfolio/component/portfolio-modal/component/transaction-card/transaction-card.component.ts
+++ src/app/portfolio/component/portfolio-modal/component/transaction-card/transaction-card.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 import { TransactionType } from 'src/app/enums';
-import { Transaction } from 'src/app/interface';
+import { PortfolioState, Transaction } from 'src/app/interface';
+import { AddTransactionComponent } from 'src/app/portfolio/modals/add-transaction/add-transaction.component';
+import { deleteTransaction } from 'src/app/portfolio/store/portfolio.actions';
 
 @Component({
   selector: 'app-transaction-card',
@@ -9,25 +13,39 @@ import { Transaction } from 'src/app/interface';
 })
 export class TransactionCardComponent implements OnInit {
   @Input() public transaction!: Transaction;
+  @Input() public portfolioId: number | undefined;
   public collapseCard: boolean = true;
 
-  constructor() {}
+  constructor(
+    private dialog: MatDialog,
+    private store: Store<PortfolioState>
+  ) {}
 
   ngOnInit(): void {}
 
   public toggleCollapse(): void {
-    console.log('toggle clicked');
     this.collapseCard = !this.collapseCard;
   }
 
   public editTransaction(): void {
     event?.stopPropagation();
-    console.log('edit clicked');
+    this.dialog.open(AddTransactionComponent, {
+      data: {
+        portfolioId: this.portfolioId,
+        option: 'edit',
+        transaction: this.transaction,
+      },
+      panelClass: 'transaction-modal',
+    });
   }
 
   public deleteTransaction(): void {
     event?.stopPropagation();
-    console.log('delete clicked');
+    if (this.transaction && this.transaction.id) {
+      this.store.dispatch(
+        deleteTransaction({ transactionId: this.transaction.id })
+      );
+    }
   }
 
   get transactionType() {
