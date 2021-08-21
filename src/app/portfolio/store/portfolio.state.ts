65a5d4ca334a588a140210da810ff33e40b6834f
Rikesh <recasechhunju@gmail.com>
2021-08-21 22:43:54 +0545

    Update portfolio state with company list and api actions

--- src/app/portfolio/store/portfolio.state.ts
+++ src/app/portfolio/store/portfolio.state.ts
@@ -3,4 +3,7 @@ import { PortfolioState } from 'src/app/interface';
 export const portfolioState: PortfolioState = {
   userPortfolioData: null,
   portfolioApiLoading: false,
+  companyList: null,
+  createPortfolioApiLoading: false,
+  transactionApiLoading: false,
 };
