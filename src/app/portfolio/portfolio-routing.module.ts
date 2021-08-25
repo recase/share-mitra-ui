86df187bc62b74cbd07aae391739e35146c0ce48
Rikesh <recasechhunju@gmail.com>
2021-08-25 19:31:24 +0545

    Add router link to alert, watchlist and targetloss

--- src/app/portfolio/portfolio-routing.module.ts
+++ src/app/portfolio/portfolio-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlertListComponent } from './component/alert-list/alert-list.component';
 import { PortfolioListComponent } from './component/portfolio-list/portfolio-list.component';
+import { TargetLossComponent } from './component/target-loss/target-loss.component';
+import { WatchListComponent } from './component/watch-list/watch-list.component';
 import { PortfolioComponent } from './portfolio.component';
 
 const routes: Routes = [
@@ -11,6 +14,18 @@ const routes: Routes = [
       {
         path: '',
         component: PortfolioListComponent,
+      },
+      {
+        path: 'alerts',
+        component: AlertListComponent,
+      },
+      {
+        path: 'watch-list',
+        component: WatchListComponent,
+      },
+      {
+        path: 'target-stop-loss',
+        component: TargetLossComponent,
       },
     ],
   },
